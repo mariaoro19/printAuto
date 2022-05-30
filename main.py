26def4f284436027017c5f57bdb614e8ab235c0a
mariaoro19 <mariao.1994@gmail.com>
2022-05-30 21:54:32 +0100

    InitialComments

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-#from flask import Flask
+# Libreries
 import datetime
 import sys
 import os
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-#import tkinter as tk
-#from tkinter import ttk
 import cups
-from tkinter import *
-from tkinter.ttk import *
-from tkinter.filedialog import askopenfile 
 import time
 
-#ws = Tk()
-#ws.title('PythonGuides')
-#ws.geometry('400x200') 
-#app = Flask(__name__)
-#@app.route('/')
-#def index():
- #   return 'Hello world'
+# Initialing API
 app = Flask(__name__)
+
+# Calling principal html index
 @app.route("/")
 def hello():
    now = datetime.datetime.now()
@@ -29,26 +20,14 @@ def hello():
       'time': timeString
       }
    return render_template('index.html', **templateData)
-@app.route('/my-link/')
-def my_link():
-  conn = cups.Connection ()
-  printers = conn.getPrinters ()
-  for printer in printers:
-      print (printer, printers[printer]["device-uri"])
-      printer_name=printer
-  print(printer_name)
-  file = "impriir.txt"
-  print(printer)
-  conn.printFile (printer_name, file, "Project Report", {})  
-
-  return 'Click.'
 
 @app.route('/upload')
 def upload_file():
    return render_template('index.html')
-	
+
+# Printing after uploading the file	
 @app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file_2():
+def send_file():
    print("uploader")
    if request.method == 'POST':
       f = request.files['file']
@@ -60,11 +39,10 @@ def upload_file_2():
          printer_name=printer
       print(f.filename)
       file =f.filename
-      print(printer)
       conn.printFile (printer_name, file, "Project Report", {})  
       return 'file uploaded successfully'
 
-
+# Connecting to the localhost
 if __name__ == '__main__':
 
    app.run(debug=True, port=3003, host='192.168.1.15')
